82d8fc5b85ea40dfd6be40a627ced2666bf5af75
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2025-03-18 20:44:07 +0200

    add layers modal

---
+++ Misc/ModalController.cs
@@ -0,0 +1,16 @@
+﻿using AbstractImagesGenerator.Components;
+using AbstractImagesGenerator.Pages;
+
+namespace AbstractImagesGenerator.Misc
+{
+    public static class ModalController
+    {
+        private static LayersModal Modal => Home.Modal;
+
+        public static async Task<Layer?> ShowAsync(bool onlyBlendings)
+        {
+            Modal.SetLayers(await Blending.GetLayerOptions(), (onlyBlendings ? [] : await Drawing.GetLayerOptions()));
+            return await Modal.ShowAsync();
+        }
+    }
+}
