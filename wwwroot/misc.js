09d55fc2bd2bd6e03d10bd4c233aec62f8c8864c
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2025-03-21 23:03:58 +0200

    add custom color picker

---
+++ wwwroot/misc.js
@@ -0,0 +1,8 @@
+﻿window.isBottomPart = (y) => {
+    console.log(y, window.innerHeight / 2);
+    return y > window.innerHeight / 2;
+};
+
+window.fitsToTheRight = (x, size) => {
+    return x + size < window.innerWidth;
+}
