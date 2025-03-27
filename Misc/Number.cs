785503200b39e4461ed6ddb954c46e517767e66a
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2025-03-27 16:10:13 +0200

    fix chrome range value

---
+++ Misc/Number.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace AbstractImagesGenerator.Misc
+{
+    public record Number(double Value) : IComparable
+    {
+        public Number(string s) : this(Parse(s)){}
+
+        private static double Parse(string s)
+        {
+            if (double.TryParse(s.Replace(',', '.'), out var result))
+            {
+                return result;
+            }
+            else if (double.TryParse(s.Replace('.', ','), out var _result))
+            {
+                return _result;
+            }
+            throw new ArgumentException($"Cannot parse {s} as a number");
+        }
+
+        public static explicit operator double(Number n) => n.Value;
+        public static explicit operator int(Number n) => (int)n.Value;
+        public static implicit operator Number(double i) => new(i);
+
+        public override string ToString() => Value.ToString().Replace(',', '.');
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is Number n)
+            {
+                return Value.CompareTo(n.Value);
+            }
+            double v = Convert.ToDouble(obj);
+            return Value.CompareTo(v);
+        }
+    }
+}
